844667dca046b367a66b957c178499207fc2d75f
sendistephen <sendi.steve2@gmail.com>
2021-05-07 23:13:48 +0300

    Update README.md file

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { config } = require('dotenv');
+const morgan = require('morgan');
 const dbConnection = require('./config/database');
-
 // configure dotenv
 config();
 
@@ -11,6 +11,9 @@ dbConnection();
 const app = express();
 
 // initialize middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json({ extended: false }));
 
 // define application routes
@@ -19,6 +22,14 @@ app.use('/api/v1', require('./routes/user'));
 // home route
 app.get('/', (req, res) =>
   res.status(200).send({ status: 200, message: 'Welcome to Rental Service!' })
+);
+
+// for un handled routes
+app.all('*', (req, res, next) =>
+  res.status(404).json({
+    status: 'Fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  })
 );
 
 // listen to port
