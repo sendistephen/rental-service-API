11f7af23099be029eaa63eeeb1e447ed001e782c
sendistephen <sendi.steve2@gmail.com>
2021-05-14 23:15:01 +0300

    Add functionality to get rentals according to city

--- server/controllers/rental.js
+++ server/controllers/rental.js
@@ -1,5 +1,10 @@
 const Rental = require('../models/rental');
 
+/**
+ * @route POST /api/v1/rentals
+ * @Access PRIVATE
+ * @description This end point adds a new rental to the database collection
+ */
 exports.create = async (req, res) => {
   const rentalData = req.body;
   rentalData.owner = res.locals.user;
@@ -10,5 +15,24 @@ exports.create = async (req, res) => {
       return res.databaseError(err);
     }
     return res.json(savedRental);
+  });
+};
+
+/**
+ * @route GET /api/v1/rentals?city="kanjokya"
+ * @Access PUBLIC
+ * @description This end point gets all the rentals from the database
+ */
+exports.getRentals = async (req, res) => {
+  // get search creteria from query params
+  const { city } = req.query;
+  // build query
+  const query = city ? { city: city.toLowerCase() } : {};
+  // find rentals from the city provided
+  await Rental.find({ query }, (err, foundRentals) => {
+    if (err) {
+      return res.databaseError(err);
+    }
+    return res.json(foundRentals);
   });
 };
--- server/routes/rental.js
+++ server/routes/rental.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { create } = require('../controllers/rental');
+const { create, getRentals } = require('../controllers/rental');
 const { authorized } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/', authorized, create);
+router.get('/', getRentals);
 
 module.exports = router;
