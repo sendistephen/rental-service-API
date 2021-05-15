30ef63260727677005e06d155f10e3665763c02a
sendistephen <sendi.steve2@gmail.com>
2021-05-15 21:38:22 +0300

    Add get rental owner bookings route

--- server/routes/booking.js
+++ server/routes/booking.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const { create } = require('../controllers/booking');
+const {
+  create,
+  getUserBookings,
+  getRecievedBookings,
+} = require('../controllers/booking');
 const { authorized } = require('../middleware/auth');
 const { checkIfUserIsOwnerOfRental } = require('../middleware');
 
 const router = express.Router();
 
+// router.get('/', authorized, getBookings);
+router.get('/me', authorized, getUserBookings);
+router.get('/recieved', authorized, getRecievedBookings);
 router.post('/', authorized, checkIfUserIsOwnerOfRental, create);
 
 module.exports = router;
